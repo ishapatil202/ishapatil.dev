7d2af0a89cb00861337a4ff70c64dcb66477c004
ishapatil202 <100125469+ishapatil202@users.noreply.github.com>
2025-06-24 12:19:15 -0700

    Update Introduction.tsx

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -7,7 +7,7 @@ const Introduction: React.FC = () => {
     <div className="introduction-page">
       <h1>Introduction</h1>
       <p>
-      I’m a Master’s student in Computer Science at CSULA, passionate about building impactful software solutions that bridge technology and real-world needs. I’m excited by opportunities across AI, data, and full-stack development—and I’m eager to take on challenges that blend creativity with code. Currently seeking internships where I can learn, contribute, and grow as a software engineer. Let’s connect!
+      I’m a Master’s student in Computer Science at CSULA, passionate about building impactful software solutions that bridge technology and real-world needs. I’m excited by opportunities across AI, data, and full-stack development—and I’m eager to take on challenges that blend creativity with code. Let’s connect!
       </p>
     </div>
   );
